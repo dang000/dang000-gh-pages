d0e0e206b27f01f94256f9f45578fb957cd9ee63
dang000 <anthonydang30@gmail.com>
2019-12-09 15:26:17 +1100

    Updates

---
+++ precache-manifest.45b15c2b790e4e644d842c6a458d7565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4aae5e6f2902bccf2afeadc71c51f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84fa11d0cbd0e3282c38",
+    "url": "/static/css/main.4104e233.chunk.css"
+  },
+  {
+    "revision": "d1eb490634a2a00a8347",
+    "url": "/static/js/2.69c8d297.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.69c8d297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84fa11d0cbd0e3282c38",
+    "url": "/static/js/main.72467622.chunk.js"
+  },
+  {
+    "revision": "d6dc1b828a195d04a523",
+    "url": "/static/js/runtime-main.505da2fc.js"
+  }
+]);
